0067fb8b949a8f241d7aa8db40a2bf14c2b81f3b
Jaspreet Singh <findjsk@gmail.com>
2024-04-26 08:28:05 +1000

    remove test cases

--- lib/groups/bloc/groups_bloc.dart
+++ lib/groups/bloc/groups_bloc.dart
@@ -1,7 +1,4 @@
-import 'dart:html';
-
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
 import 'package:transactions_repository/transactions_repository.dart';
 import 'package:equatable/equatable.dart';
 
@@ -9,12 +6,11 @@ part 'groups_event.dart';
 part 'groups_state.dart';
 
 class GroupsBloc extends Bloc<GroupsEvent, GroupsState> {
-  GroupsBloc({
-    required TransactionsRepository transactionsRepository
-    }) : _transactionsRepository=transactionsRepository, super(GroupsState()) {
-
+  GroupsBloc({required TransactionsRepository transactionsRepository})
+      : _transactionsRepository = transactionsRepository,
+        super(GroupsState()) {
     on<GroupsFetched>((event, emit) async {
-      List<Group> groups = await  _transactionsRepository.getGroups();
+      List<Group> groups = await _transactionsRepository.getGroups();
       emit(GroupsState(status: GroupsStatus.success, groups: groups));
     });
   }
