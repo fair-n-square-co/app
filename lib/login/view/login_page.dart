671f4d194fa593b4bfb21db7b979e6ad8adc439b
Abhishek Rana <abhishek.rana@shinesolutions.com>
2024-05-16 19:17:09 +1000

    add sign up feature

--- lib/login/view/login_page.dart
+++ lib/login/view/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:authentication_repository/authentication_repository.dart';
 import 'package:fair_and_square/login/view/login_form.dart';
+import 'package:fair_and_square/sign_up/view/sign_up_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fair_and_square/login/login.dart';
@@ -15,10 +16,34 @@ class LoginPage extends StatelessWidget {
       appBar: AppBar(title: const Text('Login')),
       body: Padding(
         padding: const EdgeInsets.all(8),
-        child: BlocProvider(
-          create: (_) => LoginCubit(context.read<AuthenticationRepository>()),
-          child: const LoginForm(),
-        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Expanded(
+              child: BlocProvider(
+                      create: (_) => LoginCubit(context.read<AuthenticationRepository>()),
+                      child: const LoginForm(),
+                    ),
+            ),
+            const SizedBox(height: 16),
+            GestureDetector( // Wrap with GestureDetector
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const SignUpPage()
+                  )
+                );
+              },
+              child: Text( // Use Text widget
+                'New user? Click here to Sign Up',
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor, // Use the primary color for text
+                  decoration: TextDecoration.underline, // Underline the text
+                ),
+              ),
+            ),
+          ],
+        )
       ),
     );
   }
